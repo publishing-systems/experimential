92cfdc747f13379c6ff376df258f607e85151bbf
skreutzer <stephankreutzer@gmx.net>
2017-07-25 08:58:29 +0200

    Now dragdrop.html really works.

--- nodejs-local/install.sh
+++ nodejs-local/install.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
+# Copyright (C) 2017 Stephan Kreutzer
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License version 3 or any later version,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License 3 for more details.
+#
+# You should have received a copy of the GNU Affero General Public License 3
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 sudo apt-get install nodejs
 npm install express
